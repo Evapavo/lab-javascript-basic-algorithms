9f87f82a4bad014a13ed85e46b56a1d5998799cc
Evapavo <32174314+Evapavo@users.noreply.github.com>
2017-10-24 21:44:11 +0200

    names and input

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,8 @@
 // Names and Input
-
+var hacker1 = "Eva";
+console.log("The driver's name is " + hacker1);
+var hacker2 = prompt("What's your name?");
+console.log("The navigator is  " + hacker2);
 
 //Conditionals
 
